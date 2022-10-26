39ffda60ca8d4a5e73ca20a5bcf3f127a3d4957e
student <student@IPS1>
2022-10-26 18:11:56 +0400

    Update 2

--- Source.cpp
+++ Source.cpp
@@ -16,7 +16,9 @@ class broken_line
 {
 	point* line = NULL;
 	int size, count, grow;
+
 public:
+
 	broken_line(int _size = 0, int _grow = 5) : size(_size), grow(_grow)
 	{
 		if (_size < 0) throw "Error! Array size cannot be negative";
@@ -29,6 +31,7 @@ public:
 			line[i].y = 0;
 		}
 	}
+
 	broken_line(const broken_line& v)
 	{
 		size = v.size;
@@ -41,16 +44,19 @@ public:
 			line[i].y = v.line[i].y;
 		}
 	}
+
 	~broken_line()
 	{
 		clear();
 	}
+
 	void clear()
 	{
 		free(line);
 		size = 0;
 		count = 0;
 	}
+
 	broken_line& operator = (const broken_line& v)
 	{
 		clear();
@@ -66,13 +72,13 @@ public:
 		}
 		return *this;
 	}
+
 	broken_line& operator += (const broken_line& v)
 	{
-		while (size < count + v.count) size += grow;
-
+		if (size - (count + v.count) > grow) size = count + v.count;
+		else size += grow;
 		point* tmp = (point*)realloc(line, sizeof(point) * size);
 		if (tmp != NULL) line = tmp;
-
 		for (int i = 0; i < v.count; i++)
 		{
 			line[i + count].x = v.line[i].x;
@@ -81,12 +87,14 @@ public:
 		count += v.count;
 		return *this;
 	}
+
 	broken_line operator + (const broken_line& v)
 	{
 		broken_line temp(*this);
 		temp += v;
 		return temp;
 	}
+
 	broken_line& operator += (const point& v)
 	{
 		if (count + 1 > size) size += grow;
@@ -96,12 +104,14 @@ public:
 		line[count++].y = v.y;
 		return *this;
 	}
+
 	broken_line operator + (const point& v)
 	{
 		broken_line temp(*this);
 		temp += v;
 		return temp;
 	}
+
 	point& operator[] (const int index)
 	{
 		if (index < count && count != 0) return line[index];
@@ -115,6 +125,7 @@ public:
 		for (int i = 0; i < count - 1; i++) rez += sqrt(pow((line[i + 1].x - line[i].x), 2) + pow((line[i + 1].y - line[i].y), 2));
 		return rez;
 	}
+
 	void print()
 	{
 		cout << "size: " << size << endl << "count: " << count << endl;
