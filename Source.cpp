191e3b5f7c84d9a326860a3e5cc97a844239b402
student <student@IPS1>
2022-10-26 17:06:38 +0400

    Add menu

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,7 @@
 #include<cstdio>
 #include<iostream>
+#include<conio.h>
+#include<windows.h>
 #include"Graphics.h"
 using namespace std;
 
@@ -108,8 +110,8 @@ public:
 
 	double calculate()
 	{
-		if (count == 0) throw "Error! Line has no vertices";
 		double rez = 0;
+		if (count < 2) return rez;
 		for (int i = 0; i < count - 1; i++) rez += sqrt(pow((line[i + 1].x - line[i].x), 2) + pow((line[i + 1].y - line[i].y), 2));
 		return rez;
 	}
@@ -146,5 +148,97 @@ broken_line operator + (const point& p, const broken_line& v)
 
 int main()
 {
-	
+	broken_line a(5);
+	broken_line b(5);
+	b[0].x = 4;
+	b[3].y = 6;
+	point f;
+	f.x = 4;
+	f.y = 5;
+	while (true)
+	{
+		system("cls");
+		cout << "1 - Enter value by index" << endl;
+		cout << "2 - Obj broken_list + obj broken_list" << endl;
+		cout << "3 - Obj broken_list + obj point" << endl;
+		cout << "4 - Obj point + obj broken_list" << endl;
+		cout << "5 - Calculate" << endl;
+		cout << "6 - Tack" << endl;
+		cout << "7 - Exit" << endl;
+		char z = getch();
+		if (z == '1')
+		{
+			int index, value_x, value_y;
+			cout << "Enter index" << endl;
+			cin >> index;
+			cout << "Enter value for x" << endl;
+			cin >> value_x;
+			cout << "Enter value for y" << endl;
+			cin >> value_y;
+			try
+			{
+				a[index].x = value_x;
+				a[index].y = value_y;
+			}
+			catch (const char* msg)
+			{
+				cout << msg << endl;
+				getch();
+			}
+			system("cls");
+		}
+		if (z == '2')
+		{
+			cout << "line a" << endl;
+			a.print();
+			cout << "line b" << endl;
+			b.print();
+			broken_line d = a + b;
+			cout << "line a + line b" << endl;
+			d.print();
+			getch();
+		}
+		if (z == '3')
+		{
+			cout << "line a" << endl;
+			a.print();
+			cout << "point f" << endl << f.x << " " << f.y <<endl;
+			broken_line d = a + f;
+			cout << "line a + point f" << endl;
+			d.print();
+			getch();
+		}
+		if (z == '4')
+		{
+			cout << "line a" << endl;
+			a.print();
+			cout << "point f" << endl << f.x << " " << f.y << endl;
+			broken_line d = f + a;
+			cout << "point f + line a" << endl;
+			d.print();
+			getch();
+		}
+		if (z == '5')
+		{
+			cout << "line a" << endl;
+			a.print();
+			cout << "lengh line a: " << a.calculate();
+			getch();
+		}
+		if (z == '6')
+		{
+			broken_line t(4);
+			t[0].x = 150;
+			t[0].y = 30;
+			t[1].x = 100;
+			t[1].y = 30;
+			t[2].x = 100;
+			t[2].y = 100;
+			t[3].x = 150;
+			t[3].y = 100;
+			t.print_graf();
+			getch();
+		}
+		if (z == '7') return 0;
+	}
 }
