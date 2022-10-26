dc3c034b4f127e987a16ddfc69ca6b36b3d664e8
student <student@IPS1>
2022-10-26 16:23:34 +0400

    Add constructors

--- Source.cpp
+++ Source.cpp
@@ -14,7 +14,40 @@ class broken_line
 {
 	point* line = NULL;
 	int size, count, grow;
+public:
+	broken_line(int _size = 0, int _grow = 5) : size(_size), grow(_grow)
+	{
+		if (_size < 0) throw "Error! Array size cannot be negative";
+		if (_grow < 0) throw "Error! grow cannot be negative";
+		line = (point*)malloc(sizeof(point) * size);
+		count = size;
+		for (int i = 0; i < count; i++)
+		{
+			line[i].x = 0;
+			line[i].y = 0;
+		}
+	}
+	broken_line(const broken_line& v)
+	{
+		size = v.size;
+		count = v.count;
+		grow = v.grow;
+		line = (point*)malloc(sizeof(point) * size);
+		for (int i = 0; i < count; i++)
+		{
+			line[i].x = v.line[i].x;
+			line[i].y = v.line[i].y;
+		}
+
+	}
 };
+
+
+
+
+
+
+
 
 
 
